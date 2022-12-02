6fefd35ae6a87b525c837fd90a847b99162b2e39
Ishwari <ishwarikulkarni23@gmail.com>
2022-12-02 13:45:03 +0530

    changes

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let songIndex=0;
-let audioElement=new Audio('Rasiya.mp3');
+let audioElement=new Audio('songs/1.mp3');
 let masterPlay=document.getElementById('masterPlay');
 let myProgressBar=document.getElementById('myProgressBar');
-
+let songItems=document.getElementsByClassName('songItem');
 
 let songs=[
-    {songName: "Salam-e-Ishq", filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath:"song/1.mp3",coverPath:"covers/1.jpg"}
+    {songName: "Awargi", filePath:"songs/2.mp3",coverPath:"covers/2.jpg"},
+    {songName: "Kesariya", filePath:"songs/3.mp3",coverPath:"covers/3.jpg"},
+    {songName: "Galliyan Returns", filePath:"songs/4.mp3",coverPath:"covers/4.jpg"},
+    {songName: "Raatan Lambiyaan", filePath:"songs/5.mp3",coverPath:"covers/5.jpg"},
+    {songName: "Baarish Aayi Hai", filePath:"songs/6.mp3",coverPath:"covers/6.jpg"},
+    
 ]
 
 
@@ -26,4 +26,61 @@ masterPlay.addEventListener('click', ()=>{
         masterPlay.classList.add('fa-play-circle');
     }
 
+})
+
+audioElement.addEventListener('timeupdate', ()=>{
+    progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value=progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime=myProgressBar.value*audioElement.duration/100;
+})
+
+const makeAllPlays=()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) =>{
+        element.classList.remove('fa-circle-pause');
+        element.classList.add('fa-circle-play');
+    })  
+}
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+    element.addEventListener('click', (e)=>{
+        makeAllPlays();
+        songIndex=parseInt(e.target.id);
+        e.target.classList.remove('fa-circle-play');
+        e.target.classList.add('fa-circle-pause');
+        audioElement.src=`songs/${songIndex}.mp3`;
+        audioElement.currentTime=0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', () =>{
+    if(songIndex>=7){
+        songIndex=0;
+    }
+    else{
+        songIndex+=1;
+    }
+    audioElement.src=`songs/${songIndex}.mp3`;
+    audioElement.currentTime=0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
+
+document.getElementById('previous').addEventListener('click', () =>{
+    if(songIndex<=0){
+        songIndex=0;
+    }
+    else{
+        songIndex-=1;
+    }
+    audioElement.src=`songs/${songIndex}.mp3`;
+    audioElement.currentTime=0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
 })
